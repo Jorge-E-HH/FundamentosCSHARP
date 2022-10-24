4204a9514500c3002ed488d6b6008b6ef492febf
ThethingOne <Jaydersmk@gmail.com>
2022-10-24 14:26:57 -0500

    leccion 1

---
+++ FundamentosCSHARP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FundamentosCSHARP
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persona =
+                new { nombre = "Jorge", apellido = "Hernández", edad = "26"};
+            Console.WriteLine(persona.edad);
+        }
+    }
+
+}
